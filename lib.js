6e7fb9d680cede5069d10444b3483fedd8abe128
Simon Schraeder <info@simon-schraeder.de>
2016-06-09 18:53:29 +0200

    Fixes #2: Actually use API key

--- lib.js
+++ lib.js
@@ -7,8 +7,8 @@ function FootballData(apiToken) {
   this.apiToken = apiToken;
 }
 
-function getRequest(path) {
-  return {
+FootballData.prototype.getRequest = function (path) {
+  return = {
     uri: API_URL + path,
     headers: {
         'X-Auth-Token': this.apiToken
@@ -18,40 +18,40 @@ function getRequest(path) {
 }
 
 FootballData.prototype.getSeasons = function () {
-  return rp(getRequest('/v1/soccerseasons/'));
+  return rp(this.getRequest('/v1/soccerseasons/'));
 }
 
 FootballData.prototype.getTeamsInSeason = function (seasonId) {
-  return rp(getRequest(`/v1/soccerseasons/${seasonId}/teams`));
+  return rp(this.getRequest(`/v1/soccerseasons/${seasonId}/teams`));
 }
 
 FootballData.prototype.getLeagueTable = function (seasonId) {
-  return rp(getRequest(`/v1/soccerseasons/${seasonId}/leagueTable`));
+  return rp(this.getRequest(`/v1/soccerseasons/${seasonId}/leagueTable`));
 }
 
 FootballData.prototype.getLeagugeFixtures = function (seasonId) {
-  return rp(getRequest(`/v1/soccerseasons/${seasonId}/fixtures`));
+  return rp(this.this.getRequest(`/v1/soccerseasons/${seasonId}/fixtures`));
 }
 
 FootballData.prototype.getFixtures = function () {
-  return rp(getRequest(`/v1/fixtures`));
+  return rp(this.getRequest(`/v1/fixtures`));
 }
 
 FootballData.prototype.getFixture = function (fixtureId) {
-  return rp(getRequest(`/v1/fixtures/${seasonId}`));
+  return rp(this.getRequest(`/v1/fixtures/${seasonId}`));
 }
 
 FootballData.prototype.getTeam = function (teamId) {
-  return rp(getRequest(`/v1/teams/${seasonId}`));
+  return rp(this.getRequest(`/v1/teams/${seasonId}`));
 }
 
 FootballData.prototype.getTeamPlayers = function (teamId) {
-  return rp(getRequest(`/v1/teams/${seasonId}/players`));
+  return rp(this.getRequest(`/v1/teams/${seasonId}/players`));
 }
 
 
 FootballData.prototype.getTeamFixtures = function (teamId) {
-  return rp(getRequest(`/v1/teams/${seasonId}/fixtures`));
+  return rp(this.getRequest(`/v1/teams/${seasonId}/fixtures`));
 }
 
 module.exports = FootballData;
